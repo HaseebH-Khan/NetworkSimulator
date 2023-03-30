3c6b7a66b86243ed098d5c9acc252dbdb6dbbcd6
HaseebH-Khan <haseebk841@gmail.com>
2023-03-30 14:31:08 +0530

    Basic Physical Layer Developed

--- src/main/java/net/networkSimulator/model/devices/EndDevice.java
+++ src/main/java/net/networkSimulator/model/devices/EndDevice.java
@@ -1,27 +1,42 @@
 package main.java.net.networkSimulator.model.devices;
 
 public class EndDevice {
-    int end_id;
+    static int counter = 0;
+    int dev_id; // ensure it is unique and not garbage ... not done yet
     int portN;
-    Port ports[] = new Port[portN];
+    Port[] ports;
     public static void main(String[] args) {
-
+        EndDevice dev1 = new EndDevice(2);
+        EndDevice dev2 = new EndDevice(2);
+        Wire wire1 = new Wire(dev1.ports[0], dev2.ports[0]);
+        dev1.send("Hello");
+        Wire wire2 = new Wire(dev1.ports[1], dev2.ports[0]);
+        dev2.send("Hell");
+        wire2.end1 = dev1.ports[1]; // needed to do this because a wire 2 is a stranded wire ... fix needed ==> wire to be destroyed
+        dev2.ports[1].connect(wire2, dev1.ports[1]);
+        dev2.send("Hi");
     }
     EndDevice(int portN) {
+        this.portN = portN;
+        ports = new Port[portN];
+        counter++;
+        this.dev_id = counter;
         for(int i=0; i<portN; i++) {
+            ports[i] = new Port();
             ports[i].port_id = i;
+            ports[i].dev = this;
         }
     }
     public void send(String message) {
+        System.out.println("Device " + this.dev_id + " broadcasted message: " + message);
         for(int i=0; i<portN; i++) {
             if(ports[i].connected == true) {
-                ports[i].msg = message;
-                ports[i].send();
+                ports[i].send(message);
             }
         }
         
     }
-    public void read() {
-
+    public void read(String message) {
+        System.out.println("Device " + this.dev_id + " received message: " + message);
     }
 }
--- src/main/java/net/networkSimulator/model/devices/Port.java
+++ src/main/java/net/networkSimulator/model/devices/Port.java
@@ -1,24 +1,41 @@
 package main.java.net.networkSimulator.model.devices;
 
 public class Port {
-    int port_id; // loc
+    EndDevice dev;
+    int port_id; // port id wrt device
     boolean connected = false;
-    Port port; 
-    // port to port filhal
-    // public void send(String message) {
-        
-    // }
-    // public void read() {
-
-    // }
-    public void connect(Port port2) {
-        Port port2 = new Port();
-        // this.port = port2;
-        this.connected = true;
-        port2.port = this;
-        port2.connected = true;
+    Wire wire;
+    boolean idle = true;
+    public void send(String message) {
+        this.wire.send(message, this);
     }
-    // public void disconnect() {
-    //     // has to disconnect and dissolve wire
-    // }
+    public void read(String message) {
+        System.out.println("Port " + this.port_id + " received the message and sent it to the device");
+        this.dev.read(message);
+    }
+    // this method exchanges the wire reference between the two ports
+    public void connect(Wire wire, Port port2) {
+        this.wire = wire;
+        this.connected = true;
+        port2.wire = wire; // redundant
+        port2.connected = true; // redundant
+        if(wire.end1 == port2) {
+            wire.end2.connected = false;
+            wire.end2.idle = true;
+            wire.end2 = this;
+        }
+        else if(wire.end2 == port2) {
+            wire.end1.connected = false;
+            wire.end1.idle = true;
+            wire.end1 = this;
+        }
+        else {
+            System.out.println("Port not found");
+        }
+    }
+    public void disconnect() {
+        connected = false;
+        wire.disconnect();
+        // destoy wire reference
+    }
 }
